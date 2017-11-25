a1f18dc5ddd995c47b03c539f4524239dc94a27a
Rawrz <astarisk@gmail.com>
2017-11-24 20:29:39 -0500

    Added in basic key status and decoding.

--- Main.py
+++ Main.py
@@ -38,8 +38,49 @@ user32 = ctypes.WinDLL('user32', use_last_error=True)
 
 ULONG_PTR = POINTER(ULONG)
 NULL = c_int(0)
+
 WH_KEYBOARD_LL = c_int(13)  # WH_KEYBOARD_LL == 13
 
+WM_KEYDOWN = 0X0100
+WM_KEYUP = 0X0101
+WK_SYSKEYDOWN = 0X0104
+WK_SYSKEYUP = 0X0105
+
+key_identifier = {
+    WM_KEYDOWN: 'Key Down',
+    WM_KEYUP: 'Key Up',
+    WK_SYSKEYDOWN: 'Key Down',
+    WK_SYSKEYUP: 'Key up'
+}
+
+virtual_keys = {
+    0x41: 'a',
+    0x42: 'b',
+    0x43: 'c',
+    0x44: 'd',
+    0x45: 'e',
+    0x46: 'f',
+    0x47: 'g',
+    0x48: 'h',
+    0x49: 'i',
+    0x4A: 'j',
+    0x4B: 'k',
+    0x4C: 'l',
+    0x4D: 'm',
+    0x4E: 'n',
+    0x4F: 'o',
+    0x50: 'p',
+    0x51: 'q',
+    0x52: 'r',
+    0x53: 's',
+    0x54: 't',
+    0x55: 'u',
+    0x56: 'v',
+    0x57: 'w',
+    0x58: 'x',
+    0x59: 'y',
+    0x5A: 'z',
+}
 # Invoke Windows API SetWindowsHookEx, CallNextHookEx, UnhookWindowsHookEx, and GetModuleHandle.
 
 # HHOOK WINAPI SetWindowsHookEx(_In_ int idHook, _In_ HOOKPROC lpfn, _In_ HINSTANCE hMod,_In_ DWORD dwThreadId);
@@ -55,14 +96,21 @@ CallNextHookEx.restype = c_int  # Keyboard events seem to return an int
 UnhookWindowsHookEx = user32.UnhookWindowsHookEx
 
 #LRESULT CALLBACK LowLevelKeyboardProc(
-#  _In_ int    nCode,
-#  _In_ WPARAM wParam,
+#  _In_ int    nCode, -- If nCode is less than zero, the hook procedure must pass the message to the CallNextHookEx
+#  _In_ WPARAM wParam,-- The identifier of the keyboard message.
+#  This parameter can be one of the following messages: WM_KEYDOWN, WM_KEYUP, WM_SYSKEYDOWN, or WM_SYSKEYUP.
+
 #  _In_ LPARAM lParam -- Pointer to KBDLLHOOKSTRUCT
 #);
+
+
 def low_level_handler(nCode, wParam, lParam):
-    print('n: ' + str(nCode))
-    print('w: ' + str(wParam))
-    print('l: ' + str(lParam) + ' ' + str(lParam.contents.vk_code))
+    if lParam.contents.vk_code in virtual_keys:
+        print('n: ' + str(nCode))
+        print('w: ' + str(key_identifier[wParam]))
+        print('l: ' + str(lParam) + ' ' + virtual_keys[lParam.contents.vk_code])
+
+    print("Call next hook")
     # TODO: Read the docs and make this CallNextHook proper
     return CallNextHookEx(NULL, nCode, wParam, lParam)
 
@@ -86,5 +134,4 @@ atexit.register(UnhookWindowsHookEx, keyboardcallback)
 
 while True:
     msg = windll.user32.GetMessageW(None, 0, 0, 0)
-    windll.user32.TranslateMessage(byref(msg))
-    windll.user32.DispatchMessageW(byref(msg))
+
