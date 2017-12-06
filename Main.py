451c3592ce3a9bec6178d35683c3d59aba1b0a0e
Rawrz <astarisk@gmail.com>
2017-12-06 14:26:18 -0500

    Started work on the GUI element of the program.

--- Main.py
+++ Main.py
@@ -15,6 +15,20 @@ class MainFrame(wx.Frame):
         wx.Frame.__init__(self, None, title="Otter Share")
         panel = wx.Panel(self)
 
+        nb = wx.Notebook(panel)
+        tab1 = Tab(nb)
+        tab2 = Tab(nb)
+        tab3 = Tab(nb)
+        tab4 = Tab(nb)
+        nb.AddPage(tab1, "Tab1")
+        nb.AddPage(tab2, "Tab2")
+        nb.AddPage(tab3, "Tab3")
+        nb.AddPage(tab4, "Tab4")
+        # Set noteboook in a sizer to create the layout
+        sizer = wx.BoxSizer()
+        sizer.Add(nb, 1, wx.EXPAND)
+        panel.SetSizer(sizer)
+
         self.tbIcon = TaskBarIcon(self)
 
         self.Bind(wx.EVT_ICONIZE, self.OnMinimize)
@@ -26,12 +40,13 @@ class MainFrame(wx.Frame):
         WinKeyboard.add_handler(screenshot_handler)
         # Start listening on keystrokes
         self.worker = None
-
+        # Split the Message Pumping onto its own thread.
         if not self.worker:
             self.worker = threading.Thread(target=WinKeyboard.listener())
-            self.worker.start()
+            #self.worker.start()
 
     def OnClose(self, event):
+        # Sends a stop pumping message
         WinKeyboard.stop_pumping(threading.get_ident())
         self.tbIcon.RemoveIcon()
         self.tbIcon.Destroy()
@@ -41,6 +56,17 @@ class MainFrame(wx.Frame):
         if self.IsIconized():
             self.Hide()
 
+
+class Tab(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent)
+        #t = wx.StaticText(self, -1, "This is a tab", (20, 20))
+        sizer = wx.BoxSizer()
+        self.SetSizer(sizer)
+        box = wx.StaticBox(self, wx.ID_ANY, "StaticBox")
+        sizer.Add(box, 1, wx.EXPAND)
+        text = wx.StaticText(box, wx.ID_ANY, "This window is a child of the staticbox")
+        text = wx.StaticText(box, wx.ID_ANY, "Meh")
 
 def print_event(event):
     print(event)
