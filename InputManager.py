8b2814995a3e6d02ee035b185902420b351470d5
Rawrz <astarisk@gmail.com>
2017-12-07 00:38:47 -0500

    Removed junk code that resided in the InputManager class.

--- InputManager.py
+++ InputManager.py
@@ -1,48 +1,7 @@
-import ctypes
-from ctypes import c_int, CFUNCTYPE, POINTER
-from ctypes.wintypes import DWORD, BOOL, HHOOK, MSG, LPARAM, ULONG, POINT, UINT, HWND
 import atexit
 import win32con
 import win32gui
 import win32api
-
-# Let's see if I can get hooks working through my own efforts and not libraries.
-# Time to clutch tightly to the msdn... and pray It'll all work.
-# https://msdn.microsoft.com/en-us/library/windows/desktop/ms632589(v=vs.85).aspx
-user32 = ctypes.WinDLL('user32', use_last_error=True)
-
-ULONG_PTR = POINTER(ULONG)
-NULL = c_int(0)
-LPMSG = POINTER(MSG)
-# Invoke Windows API SetWindowsHookEx, CallNextHookEx, UnhookWindowsHookEx, and GetModuleHandle.
-
-# HHOOK WINAPI SetWindowsHookEx(_In_ int idHook, _In_ HOOKPROC lpfn, _In_ HINSTANCE hMod,_In_ DWORD dwThreadId);
-# https://msdn.microsoft.com/en-us/library/ms644990(VS.85).aspx
-SetWindowsHookEx = user32.SetWindowsHookExA  # # ExA = ANSI -- ExW = Unicode
-SetWindowsHookEx.restype = HHOOK  # restype sets the return type, by default it assume C int
-
-# LRESULT WINAPI CallNextHookEx(_In_opt_ HHOOK  hhk, _In_ int    nCode, _In_ PARAM wParam, _In_ LPARAM lParam);
-# https://msdn.microsoft.com/en-us/library/ms644974(v=vs.85).aspx
-CallNextHookEx = user32.CallNextHookEx
-CallNextHookEx.restype = c_int  # Keyboard events seem to return an int
-
-# BOOL WINAPI UnhookWindowsHookEx(_In_ HHOOK hhk);
-# https://msdn.microsoft.com/en-us/library/ms644993(v=vs.85).aspx
-UnhookWindowsHookEx = user32.UnhookWindowsHookEx
-UnhookWindowsHookEx.argtypes = [HHOOK]
-UnhookWindowsHookEx.restype = BOOL
-
-
-# Retrieves a message from the calling thread's message queue. The function dispatches incoming sent messages
-# until a posted message is available for retrieval.
-# https://msdn.microsoft.com/en-us/library/ms644936(v=vs.85).aspx
-GetMessage = user32.GetMessageW
-GetMessage.argtypes = [LPMSG, c_int, c_int, c_int]
-GetMessage.restype = BOOL
-
-PeekMessage = user32.PeekMessageW
-PeekMessage.argtypes = [LPMSG, c_int, c_int, c_int, c_int]
-PeekMessage.restype = BOOL
 
 inputs = []
 low_level_handlers = []
